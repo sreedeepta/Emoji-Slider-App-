8f6468ac0ae2f3d92c13271e35c8a31ed96f5213
Sreedeepta Mitra <78677605+Sreedeepta@users.noreply.github.com>
2021-08-26 21:38:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const div = document.querySelector('.moji');
+const mojis = ['😄','🙂','😐','😑','☹️','😩','😠','😡','🤢','😤','🤧'];
+
+range.addEventListener('input', (e) => {
+  let rangeValue = e.target.value;
+  div.textContent = mojis[rangeValue];
+});
